f1b96c89620e3aaa7da4809fa0636c5165cd8915
Thog <me@thog.eu>
2018-10-22 17:18:56 +0200

    Add composite and arithmetic instructions

---
+++ Module/Instruction/Arithmetic.cs
@@ -0,0 +1,151 @@
+﻿using static Spv.Specification;
+
+namespace Spv.Generator
+{
+    public partial class Module
+    {
+        private Instruction EmitArithmeticInstruction(Op Opcode, uint ResultType, params uint[] Values)
+        {
+            Instruction Result = CreateInstruction(Opcode, Values);
+            Result.SetTypeId(ResultType);
+            Result.SetResultTypeId(AllocateId());
+
+            return EmitCode(Result);
+        }
+
+        public Instruction SNegate(uint ResultType, uint Operand)
+        {
+            return EmitArithmeticInstruction(Op.OpSNegate, ResultType, Operand);
+        }
+
+        public Instruction FNegate(uint ResultType, uint Operand)
+        {
+            return EmitArithmeticInstruction(Op.OpFNegate, ResultType, Operand);
+        }
+
+        public Instruction IAdd(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpIAdd, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction FAdd(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpFAdd, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction ISub(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpISub, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction FSub(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpFSub, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction IMul(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpIMul, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction FMul(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpFMul, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction UDiv(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpUDiv, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction SDiv(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpSDiv, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction FDiv(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpFDiv, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction UMod(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpUMod, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction SRem(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpSRem, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction SMod(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpSMod, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction FRem(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpFRem, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction FMod(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpFMod, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction VectorTimesScalar(uint ResultType, uint Vector, uint Scalar)
+        {
+            return EmitArithmeticInstruction(Op.OpVectorTimesScalar, ResultType, Vector, Scalar);
+        }
+
+        public Instruction MatrixTimesScalar(uint ResultType, uint Matrix, uint Scalar)
+        {
+            return EmitArithmeticInstruction(Op.OpMatrixTimesScalar, ResultType, Matrix, Scalar);
+        }
+
+        public Instruction VectorTimesMatrix(uint ResultType, uint Vector, uint Matrix)
+        {
+            return EmitArithmeticInstruction(Op.OpVectorTimesMatrix, ResultType, Vector, Matrix);
+        }
+
+        public Instruction MatrixTimesVector(uint ResultType, uint Matrix, uint Vector)
+        {
+            return EmitArithmeticInstruction(Op.OpMatrixTimesVector, ResultType, Matrix, Vector);
+        }
+
+        public Instruction MatrixTimesMatrix(uint ResultType, uint LeftMatrix, uint RightMatrix)
+        {
+            return EmitArithmeticInstruction(Op.OpMatrixTimesMatrix, ResultType, LeftMatrix, RightMatrix);
+        }
+
+        public Instruction OuterProduct(uint ResultType, uint Vector1, uint Vector2)
+        {
+            return EmitArithmeticInstruction(Op.OpOuterProduct, ResultType, Vector1, Vector2);
+        }
+
+        public Instruction Dot(uint ResultType, uint Vector1, uint Vector2)
+        {
+            return EmitArithmeticInstruction(Op.OpDot, ResultType, Vector1, Vector2);
+        }
+
+        public Instruction IAddCarry(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpIAddCarry, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction ISubBorrow(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpISubBorrow, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction UMulExtended(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpUMulExtended, ResultType, Operand1, Operand2);
+        }
+
+        public Instruction SMulExtended(uint ResultType, uint Operand1, uint Operand2)
+        {
+            return EmitArithmeticInstruction(Op.OpSMulExtended, ResultType, Operand1, Operand2);
+        }
+    }
+}
---
+++ Module/Instruction/Composite.cs
@@ -0,0 +1,66 @@
+﻿using static Spv.Specification;
+
+namespace Spv.Generator
+{
+    public partial class Module
+    {
+        private Instruction EmitCompositeInstruction(Op Opcode, uint ResultType, params uint[] Values)
+        {
+            Instruction Result = CreateInstruction(Opcode, Values);
+            Result.SetTypeId(ResultType);
+            Result.SetResultTypeId(AllocateId());
+
+            return EmitCode(Result);
+        }
+
+        public Instruction VectorExtractDynamic(uint ResultType, uint Vector, uint Index)
+        {
+            return EmitCompositeInstruction(Op.OpVectorExtractDynamic, ResultType, Vector, Index);
+        }
+
+        public Instruction VectorInsertDynamic(uint ResultType, uint Vector, uint Component, uint Index)
+        {
+            return EmitCompositeInstruction(Op.OpVectorInsertDynamic, ResultType, Vector, Component, Index);
+        }
+
+        public Instruction VectorShuffle(uint ResutType, uint Vector1, uint Vector2, params uint[] Components)
+        {
+            Instruction VectorShuffle = EmitCompositeInstruction(Op.OpVectorShuffle, ResutType, Vector1, Vector2);
+
+            VectorShuffle.PushOperand(Components);
+
+            return VectorShuffle;
+        }
+
+        public Instruction CompositeConstruct(uint ResultType, params uint[] Constituents)
+        {
+            return EmitCompositeInstruction(Op.OpCompositeConstruct, ResultType, Constituents);
+        }
+
+        public Instruction CompositeExtract(uint ResultType, uint Composite, params uint[] Indexes)
+        {
+            Instruction CompositeExtract = EmitCompositeInstruction(Op.OpCompositeExtract, ResultType, Composite);
+            CompositeExtract.PushOperand(Indexes);
+
+            return CompositeExtract;
+        }
+
+        public Instruction CompositeInsert(uint ResultType, uint Object, uint Composite, params uint[] Indexes)
+        {
+            Instruction CompositeInsert = EmitCompositeInstruction(Op.OpCompositeInsert, ResultType, Object, Composite);
+            CompositeInsert.PushOperand(Indexes);
+
+            return CompositeInsert;
+        }
+
+        public Instruction CopyObject(uint ResultType, uint Operand)
+        {
+            return EmitCompositeInstruction(Op.OpCopyObject, ResultType, Operand);
+        }
+
+        public Instruction Transpose(uint ResultType, uint Matrix)
+        {
+            return EmitCompositeInstruction(Op.OpTranspose, ResultType, Matrix);
+        }
+    }
+}

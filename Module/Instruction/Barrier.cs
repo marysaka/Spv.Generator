46c0a061612153e09e756a773c2f900cdb417291
Thog <me@thog.eu>
2018-10-23 19:27:33 +0200

    Add Primitive & Barrier instructions

---
+++ Module/Instruction/Barrier.cs
@@ -0,0 +1,17 @@
+﻿using static Spv.Specification;
+
+namespace Spv.Generator
+{
+    public partial class Module
+    {
+        public Instruction ControlBarrier(uint ExecutionScope, uint MemoryScope, uint Semantics)
+        {
+            return EmitCode(CreateInstruction(Op.OpControlBarrier, ExecutionScope, MemoryScope, Semantics));
+        }
+
+        public Instruction MemoryBarrier(uint ExecutionScope, uint Semantics)
+        {
+            return EmitCode(CreateInstruction(Op.OpMemoryBarrier, ExecutionScope, Semantics));
+        }
+    }
+}
---
+++ Module/Instruction/Primitive.cs
@@ -0,0 +1,27 @@
+﻿using static Spv.Specification;
+
+namespace Spv.Generator
+{
+    public partial class Module
+    {
+        public Instruction EmitVertex()
+        {
+            return EmitCode(CreateInstruction(Op.OpEmitVertex));
+        }
+
+        public Instruction EndPrimitive()
+        {
+            return EmitCode(CreateInstruction(Op.OpEndPrimitive));
+        }
+
+        public Instruction EmitStreamVertex(uint Stream)
+        {
+            return EmitCode(CreateInstruction(Op.OpEmitStreamVertex, Stream));
+        }
+
+        public Instruction EndStreamPrimitive(uint Stream)
+        {
+            return EmitCode(CreateInstruction(Op.OpEndStreamPrimitive, Stream));
+        }
+    }
+}

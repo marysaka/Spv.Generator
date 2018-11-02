ef16fc6e38ff3122d796a63166f5da9b191bf516
Thog <me@thog.eu>
2018-11-02 01:30:12 +0100

    Implement annotation instructions
    SPIR-V 1.0 ISA is now complete.

--- Module/Instruction/Annotation.cs
+++ Module/Instruction/Annotation.cs
@@ -10,6 +10,44 @@ namespace Spv.Generator
             return Instruction;
         }
 
-        // TODO: All decorations opcodes
+        public Instruction Decorate(uint Target, Decoration Decoration, params uint[] Literals)
+        {
+            Instruction Decorate = CreateInstruction(Op.OpDecorate, Target, (uint)Decoration);
+            Decorate.PushOperand(Literals);
+
+            return EmitAnnotation(Decorate);
+        }
+
+        public Instruction MemberDecorate(uint StructureType, uint Member, Decoration Decoration, params uint[] Literals)
+        {
+            Instruction MemberDecorate = CreateInstruction(Op.OpMemberDecorate, StructureType, Member, (uint)Decoration);
+            MemberDecorate.PushOperand(Literals);
+
+            return EmitAnnotation(MemberDecorate);
+        }
+
+        public Instruction DecorationGroup()
+        {
+            Instruction DecorationGroup = CreateInstruction(Op.OpDecorationGroup);
+            DecorationGroup.SetResultTypeId(AllocateId());
+
+            return EmitAnnotation(DecorationGroup);
+        }
+
+        public Instruction GroupDecorate(uint DecorationGroup, params uint[] Targets)
+        {
+            Instruction GroupDecorate = CreateInstruction(Op.OpGroupDecorate, DecorationGroup);
+            GroupDecorate.PushOperand(Targets);
+
+            return EmitAnnotation(GroupDecorate);
+        }
+
+        public Instruction GroupMemberDecorate(uint DecorationGroup, params uint[] Targets)
+        {
+            Instruction GroupMemberDecorate = CreateInstruction(Op.OpGroupMemberDecorate, DecorationGroup);
+            GroupMemberDecorate.PushOperand(Targets);
+
+            return EmitAnnotation(GroupMemberDecorate);
+        }
     }
 }

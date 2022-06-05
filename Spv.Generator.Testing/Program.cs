37797c7b044d279d11d7cd134db19e2768cc5003
Youness KAFIA <32330908+ykafia@users.noreply.github.com>
2022-06-06 00:38:53 +0200

    testing something else

--- Spv.Generator.Testing/Program.cs
+++ Spv.Generator.Testing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using static Spv.Specification;
 
@@ -31,23 +32,38 @@ namespace Spv.Generator.Testing
                 AddGlobalVariable(outputTest);
                 AddGlobalVariable(outputColor);
 
+                var inttype = TypeInt(32,1);
+                var a = Constant(inttype,1);
+                var b = Constant(inttype,3);
+
+
+                
+
                 Instruction rColor = Constant(floatType, 0.5f);
                 Instruction gColor = Constant(floatType, 0.0f);
                 Instruction bColor = Constant(floatType, 0.0f);
                 Instruction aColor = Constant(floatType, 1.0f);
-
+                
                 Instruction compositeColor = ConstantComposite(vec4Type, rColor, gColor, bColor, aColor);
+                
+
 
                 Instruction voidType = TypeVoid();
 
                 Instruction mainFunctionType = TypeFunction(voidType, true);
                 Instruction mainFunction = Function(voidType, FunctionControlMask.MaskNone, mainFunctionType);
                 AddLabel(Label());
+                var intVarType = TypePointer(StorageClass.Function, inttype);
+                var unused = Variable(intVarType,StorageClass.Function);
+                Name(unused, "unused");
+                AddLocalVariable(unused);
 
                 Instruction tempInput = Load(floatType, inputTest);
 
                 Instruction resultSqrt = GlslSqrt(floatType, tempInput);
 
+                var sum = IAdd(inttype,a,b);
+                Store(unused,sum);
                 Store(outputTest, resultSqrt);
                 Store(outputColor, compositeColor);
 
@@ -61,11 +77,14 @@ namespace Spv.Generator.Testing
 
         static void Main(string[] Args)
         {
+            var s = new Stopwatch();
+            s.Start();
             TestModule module = new TestModule();
             module.Construct();
 
             byte[] ModuleData = module.Generate();
-
+            s.Stop();
+            Console.WriteLine(s.Elapsed);
             File.WriteAllBytes(Args[0], ModuleData);
 
             Console.WriteLine(Args[0]);

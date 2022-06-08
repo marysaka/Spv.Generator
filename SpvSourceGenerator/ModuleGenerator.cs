d0c5c7579d00563cc9e807bef87a3a9b716b5d51
Youness KAFIA <32330908+ykafia@users.noreply.github.com>
2022-06-08 15:35:53 +0200

    added generator

---
+++ SpvSourceGenerator/ModuleGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace SpvSourceGenerator;
+using Microsoft.CodeAnalysis;
+using System.Text;
+
+[Generator]
+public class ModuleGenerator : ISourceGenerator
+{
+    public string pathJson = @"..\SPIRV-Headers\include\spirv\unified1\spirv.core.grammar.json";
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var compil = context.Compilation;
+        context.AddSource("Module.Generated.cs",
+        new StringBuilder().AppendLine("public partial class Module{")
+        .AppendLine("public string PathOfJson = \"{pathJson}\";}").ToString());
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        
+    }
+}
